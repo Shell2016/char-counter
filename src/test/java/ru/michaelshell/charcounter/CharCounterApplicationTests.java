cf709075eaba13c0a3414f2c860ca07a08122b3b
mihail_sheludyakov <bvg2003@bk.ru>
2023-09-14 22:02:27 +0300

    Initial commit

---
+++ src/test/java/ru/michaelshell/charcounter/CharCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.michaelshell.charcounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharCounterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
